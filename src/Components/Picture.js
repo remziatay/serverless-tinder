acaab4b62439600de9383779af7d3778ea620048
ratay <38402094+remziatay@users.noreply.github.com>
2020-10-17 22:41:03 +0200

    added reboot and stylings

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import styles from './Picture.module.css'
 
 function Picture (props) {
   const width = props.width || 300
   const height = props.height || width
   return (
-    <img alt='' src={`https://via.placeholder.com/${width}x${height}.png`}/>
+    <div className={styles.Picture}>
+      <img alt='' src={`https://via.placeholder.com/${width}x${height}.png`}/>
+    </div>
   )
 }
 
