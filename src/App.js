558f9586a831ec91cccc6d54fda86cf1df4358f2
ratay <38402094+remziatay@users.noreply.github.com>
2020-10-17 23:58:57 +0200

    added image change on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import './App.css'
 import Picture from './Components/Picture'
 
+const randomImage = () => `https://via.placeholder.com/${300 + Math.round(Math.random() * 1000)}x${300 + Math.round(Math.random() * 500)}.png`
+
 function App () {
+  const [link, setLink] = useState(randomImage())
+
+  const newLink = useCallback(() => setLink(randomImage), [])
+
   return (
     <div className="App">
       <p>INIT</p>
-      <Picture width={300 + Math.round(Math.random() * 1000)} height={300 + Math.round(Math.random() * 500)}/>
+      <Picture link={link} newLink={newLink}/>
     </div>
   )
 }
--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -2,11 +2,9 @@ import React from 'react'
 import styles from './Picture.module.css'
 
 function Picture (props) {
-  const width = props.width || 300
-  const height = props.height || width
   return (
-    <div className={styles.Picture}>
-      <img alt='' src={`https://via.placeholder.com/${width}x${height}.png`}/>
+    <div role='button' tabIndex='0' onKeyDown={null} onClick={props.newLink} className={styles.Picture}>
+      <img alt='' src={props.link}/>
     </div>
   )
 }
