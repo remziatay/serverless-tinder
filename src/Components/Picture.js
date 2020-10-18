6b9621260ebda96a86637f95438d04462fcf48dd
ratay <38402094+remziatay@users.noreply.github.com>
2020-10-18 17:21:35 +0200

    added initial animation

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
 import styles from './Picture.module.css'
+import './PictureAnimations.css'
 
 function Picture (props) {
   return (
     <div role='button' tabIndex='0' onKeyDown={null} onClick={props.newLink} className={styles.Picture}>
-      <img alt='' src={props.link}/>
+      <SwitchTransition mode='out-in'>
+        <CSSTransition
+          key={props.link}
+          timeout={500}
+          classNames="fade"
+        >
+          <img alt='' src={props.link}/>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   )
 }
