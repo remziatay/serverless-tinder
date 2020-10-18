d97964397e53c61cf2ebcd1d21bf53bd777def73
ratay <38402094+remziatay@users.noreply.github.com>
2020-10-18 23:30:59 +0200

    added like & dislike buttons and animations

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ const randomImage = () => `https://via.placeholder.com/${300 + Math.round(Math.r
 
 function App () {
   const [link, setLink] = useState(randomImage())
+  const [liking, setLiking] = useState({ liked: false, disliked: false })
 
-  const newLink = useCallback(() => setLink(randomImage), [])
+  const newLink = useCallback(() => {
+    setLink(randomImage())
+    setLiking({ liked: false, disliked: false })
+  }, [])
+  const like = useCallback(() => setLiking({ liked: true, disliked: false }), [])
+  const dislike = useCallback(() => setLiking({ liked: false, disliked: true }), [])
 
   return (
     <div className="App">
-      <p>INIT</p>
-      <Picture link={link} newLink={newLink}/>
+      <Picture link={link} newLink={newLink} liked={liking.liked} disliked={liking.disliked}/>
+      <div style={{ textAlign: 'center', padding: '1em' }}>
+        <button onClick={dislike} style={{ width: '5em' }}>NO</button>
+        <button onClick={like} style={{ width: '5em' }}>YES</button>
+      </div>
     </div>
   )
 }
--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import React, { useEffect } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import styles from './Picture.module.css'
 import './PictureAnimations.css'
 
 function Picture (props) {
+  useEffect(() => console.log(props), [props])
+
   return (
     <div role='button' tabIndex='0' onKeyDown={null} onClick={props.newLink} className={styles.Picture}>
-      <SwitchTransition mode='out-in'>
-        <CSSTransition
-          key={props.link}
-          timeout={500}
-          classNames="fade"
-        >
-          <img alt='' src={props.link}/>
-        </CSSTransition>
-      </SwitchTransition>
+      <CSSTransition
+        in={ !props.liked && !props.disliked }
+        timeout={500}
+        classNames={`fade-out-to-${props.liked ? 'right' : 'left'}`}
+        onExited={props.newLink}
+        unmountOnExit
+      >
+        {/* <p>{props.link}</p> */}
+        <img style={{ border: '1px solid red', minWidth: '200px', minHeight: '200px' }} alt='' src={props.link}/>
+      </CSSTransition>
     </div>
   )
 }
