4e72185cb302445bfdc440c6cacc0ddc30c76354
ratay <38402094+remziatay@users.noreply.github.com>
2020-11-01 15:12:15 +0100

    Updates

---
+++ precache-manifest.2c8463d1f22180a9dc4df0a5d1aa7889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4e7fe97686d04d45656bc6e3d59345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d6efddf5c6f92bd2a8",
+    "url": "/static/css/2.16359f18.chunk.css"
+  },
+  {
+    "revision": "76d291b6c32aea77eceb",
+    "url": "/static/css/main.d0fc966a.chunk.css"
+  },
+  {
+    "revision": "37d6efddf5c6f92bd2a8",
+    "url": "/static/js/2.7266ba48.chunk.js"
+  },
+  {
+    "revision": "303a28afb1a0254f6f3e2c78172a4d95",
+    "url": "/static/js/2.7266ba48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d291b6c32aea77eceb",
+    "url": "/static/js/main.e4d0ac88.chunk.js"
+  },
+  {
+    "revision": "1b4735961b2f0872fbf8",
+    "url": "/static/js/runtime-main.91a5641a.js"
+  }
+]);
