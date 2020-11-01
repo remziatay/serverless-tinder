0055467c68dd8e11317699d36f68b686f16dfd38
ratay <38402094+remziatay@users.noreply.github.com>
2020-11-01 15:43:20 +0100

    Updates

---
+++ precache-manifest.ae4be20ec1863ae77ada40a0f59a2a16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e353fa4dabb84b20515b0cc97a378c7a",
+    "url": "/serverless-tinder/index.html"
+  },
+  {
+    "revision": "734ad0f00c39b94473ca",
+    "url": "/serverless-tinder/static/css/2.16359f18.chunk.css"
+  },
+  {
+    "revision": "ad9ad2bb8bf67e8260a3",
+    "url": "/serverless-tinder/static/css/main.d0fc966a.chunk.css"
+  },
+  {
+    "revision": "734ad0f00c39b94473ca",
+    "url": "/serverless-tinder/static/js/2.3cf5bdca.chunk.js"
+  },
+  {
+    "revision": "303a28afb1a0254f6f3e2c78172a4d95",
+    "url": "/serverless-tinder/static/js/2.3cf5bdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9ad2bb8bf67e8260a3",
+    "url": "/serverless-tinder/static/js/main.f4c78bfe.chunk.js"
+  },
+  {
+    "revision": "bd52275851d28bdd40ca",
+    "url": "/serverless-tinder/static/js/runtime-main.edef7556.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.595a9e6c32b07ec48b8a330cce5faacb.js"
+  "/serverless-tinder/precache-manifest.ae4be20ec1863ae77ada40a0f59a2a16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/serverless-tinder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
